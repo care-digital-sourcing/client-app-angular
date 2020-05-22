5151a5af160bc4d1aa5be40e477d7db6f4f86602
larryoke <larryoke@yahoo.com>
2020-05-22 01:35:52 +0100

    login, dashboard and router updated

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from '../welcome/welcome.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { LoginComponent } from '../login/login.component';
+import { LogoutComponent } from '../logout/logout.component';
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent,},
   {path: 'welcome', component: WelcomeComponent,},
+  {path: 'login', component: LoginComponent,},
+  {path: 'logout', component: LogoutComponent,},
+  {path: 'dashboard', component: DashboardComponent,},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    DashboardComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
